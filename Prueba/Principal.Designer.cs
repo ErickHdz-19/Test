4963eef7c96d50c1654cf9cc3e9111f3ede6bba8
SistemasTG <edejhernandez@tecnoglobal.mx>
2024-05-31 14:53:35 -0600

    Agregar archivos de proyecto.

---
+++ Prueba/Principal.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Prueba
+{
+    partial class Examenes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dgDatos = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.cboxtipoOpe = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Examen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(86, 11);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 2;
+            this.txtid.Leave += new System.EventHandler(this.txtid_Leave);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(86, 40);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(86, 66);
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtdescripcion.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descripción";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.Green;
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnGuardar.Location = new System.Drawing.Point(285, 101);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 6;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dgDatos
+            // 
+            this.dgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgDatos.Location = new System.Drawing.Point(29, 157);
+            this.dgDatos.Name = "dgDatos";
+            this.dgDatos.Size = new System.Drawing.Size(322, 172);
+            this.dgDatos.TabIndex = 7;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnActualizar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnActualizar.Location = new System.Drawing.Point(285, 101);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 8;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnConsultar.Location = new System.Drawing.Point(285, 101);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 9;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = false;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.BackColor = System.Drawing.Color.Red;
+            this.btneliminar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btneliminar.Location = new System.Drawing.Point(285, 101);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(75, 23);
+            this.btneliminar.TabIndex = 10;
+            this.btneliminar.Text = "Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = false;
+            this.btneliminar.Click += new System.EventHandler(this.btneliminar_Click);
+            // 
+            // cboxtipoOpe
+            // 
+            this.cboxtipoOpe.AutoCompleteCustomSource.AddRange(new string[] {
+            "Guardar",
+            "Actualizar",
+            "Eliminar",
+            "Consultar"});
+            this.cboxtipoOpe.FormattingEnabled = true;
+            this.cboxtipoOpe.Items.AddRange(new object[] {
+            "Seleccione",
+            "Guardar",
+            "Eliminar",
+            "Actualizar",
+            "Consultar"});
+            this.cboxtipoOpe.Location = new System.Drawing.Point(230, 11);
+            this.cboxtipoOpe.Name = "cboxtipoOpe";
+            this.cboxtipoOpe.Size = new System.Drawing.Size(121, 21);
+            this.cboxtipoOpe.TabIndex = 11;
+            this.cboxtipoOpe.SelectedIndexChanged += new System.EventHandler(this.cboxtipoOpe_SelectedIndexChanged);
+            // 
+            // Examenes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(378, 292);
+            this.Controls.Add(this.cboxtipoOpe);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgDatos);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Examenes";
+            this.Text = "Examenes ";
+            this.Load += new System.EventHandler(this.Examenes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtdescripcion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dgDatos;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.ComboBox cboxtipoOpe;
+    }
+}
+
---
+++ Prueba/Principal.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Prueba;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Prueba
+{
+   
+    public partial class Examenes : Form
+    {
+
+        
+        public Examenes()
+        {
+            InitializeComponent();
+        }
+
+        private void Examenes_Load(object sender, EventArgs e)
+        {
+            carga();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            GuardaRegistro();
+        }
+
+        private void btneliminar_Click(object sender, EventArgs e)
+        {
+            EliminarRegistro();
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {            
+            ConsultaRegistro();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            ActualizarRegistro();
+        }
+
+        private void cboxtipoOpe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cboxtipoOpe.Text == "Guardar")
+            {
+                btnGuardar.Visible = true;            
+                btneliminar.Visible = false;
+                btnActualizar.Visible = false;
+                btnConsultar.Visible = false;
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtdescripcion.Text = "";
+                txtdescripcion.Enabled = true;
+                txtid.Enabled = true;
+                txtnombre.Enabled = true;
+            }
+            else if(cboxtipoOpe.Text == "Eliminar")
+            {
+                btneliminar.Visible = true;
+                btnGuardar.Visible = false;               
+                btnActualizar.Visible = false;
+                btnConsultar.Visible = false;
+                txtdescripcion.Enabled=false;
+                txtnombre.Enabled = false;
+                txtid.Enabled=true;
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtdescripcion.Text = "";
+            }
+            else if(cboxtipoOpe.Text == "Actualizar")
+            {
+                btnGuardar.Visible = false;
+                btneliminar.Visible = false;
+                btnActualizar.Visible = false;
+                btnActualizar.Visible = true;
+                txtid.Enabled = true;
+                txtnombre.Enabled = true;
+                txtdescripcion.Enabled = true;
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtdescripcion.Text = "";
+            }
+            else if(cboxtipoOpe.Text == "Consultar")
+            {
+                btnGuardar.Visible = false;
+                btneliminar.Visible = false;
+                btnActualizar.Visible = false;
+                btnConsultar.Visible = true;
+                txtid.Enabled= false;
+                txtnombre.Enabled = true;
+                txtdescripcion.Enabled = true;
+                txtid.Text = "";
+                txtnombre.Text = "";
+                txtdescripcion.Text = "";
+            }
+        }
+
+        private void GuardaRegistro()
+        {
+            using (var db = new RECIBOSEntities1())
+            {
+                int id = Convert.ToInt32(txtid.Text);
+                try
+                {
+                    var CodReturn = new SqlParameter("@CodigoRetorno", SqlDbType.Int);
+                    CodReturn.Direction = ParameterDirection.ReturnValue;
+
+                     db.Database.ExecuteSqlCommand("EXEC spIngresa @id, @Nombre, @Descripcion",
+                        CodReturn,
+                         new SqlParameter("@id", id),
+                        new SqlParameter("@Nombre", txtnombre.Text),
+                        new SqlParameter("@Descripcion", txtdescripcion.Text)
+                        );
+                   
+                    int codigoRetorno = (int)CodReturn.Value;
+
+                    if (codigoRetorno == 0)
+                    {
+                        MessageBox.Show("Se guardó el registro");
+                    }
+                    else
+                    {
+                        txtid.Text = "";
+                        txtnombre.Text = "";
+                        txtdescripcion.Text = "";
+                        MessageBox.Show("Ocurrío un error al guardar el registro");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ocurrió un error : {ex.Message}");
+                }
+            }
+        }
+        private void EliminarRegistro()
+        {
+            using (var db = new RECIBOSEntities1())
+            {
+                int id = Convert.ToInt32(txtid.Text);
+                try
+                {
+                    var CodReturn = new SqlParameter("@CodigoRetorno", SqlDbType.Int);
+                    CodReturn.Direction = ParameterDirection.ReturnValue;
+
+                    db.Database.ExecuteSqlCommand("EXEC spEliminar @id",
+                       CodReturn,
+                        new SqlParameter("@id", id));
+
+                    int codigoRetorno = (int)CodReturn.Value;
+
+                    if (codigoRetorno == 0)
+                    {
+                        MessageBox.Show("Se eliminó correctamente el registro");
+                    }
+                    else
+                    {
+                        txtid.Text = "";
+                        txtnombre.Text = "";
+                        txtdescripcion.Text = "";
+                        MessageBox.Show("Ocurrío un error al  el registro");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ocurrió un error : {ex.Message}");
+                }
+            }
+        }
+        
+        private void ActualizarRegistro()
+        {
+            using (var db = new RECIBOSEntities1())
+            {
+                int id = Convert.ToInt32(txtid.Text);
+                try
+                {
+                    var CodReturn = new SqlParameter("@CodigoRetorno", SqlDbType.Int);
+                    CodReturn.Direction = ParameterDirection.ReturnValue;
+
+                    db.Database.ExecuteSqlCommand("EXEC spActualizar @id, @Nombre, @Descripcion",
+                       CodReturn,
+                        new SqlParameter("@id", id),
+                       new SqlParameter("@Nombre", txtnombre.Text),
+                       new SqlParameter("@Descripcion", txtdescripcion.Text)
+                       );
+
+                    int codigoRetorno = (int)CodReturn.Value;
+
+                    if (codigoRetorno == 0)
+                    {
+                        MessageBox.Show("Se guardo correctamente el registro");
+                    }
+                    else
+                    {
+                        txtid.Text = "";
+                        txtnombre.Text = "";
+                        txtdescripcion.Text = "";
+                        MessageBox.Show("Ocurrío un error al guardar el registro");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ocurrió un error : {ex.Message}");
+                }
+            }
+        }
+
+        private void ConsultaRegistro()
+        {
+            using(var db = new RECIBOSEntities1())
+            {                
+               var registros = db.spConsultar(txtnombre.Text, txtdescripcion.Text).ToList();
+                if (registros.Count != 0)
+                {
+                    dgDatos.Visible = true;
+                    dgDatos.DataSource = registros;
+                }
+                else
+                {
+                    MessageBox.Show("No existen registros con ese nombre: "+ txtnombre.Text+ " y esa descripción "+ txtdescripcion.Text);
+                }
+            }
+        }
+        
+        private void carga()
+        {
+            btnGuardar.Visible = false;
+            btneliminar.Visible = false;
+            btnActualizar.Visible = false;
+            btnConsultar.Visible = false;
+            dgDatos.Visible = false;
+            txtdescripcion.Enabled = false;
+            txtid.Enabled = false;
+            txtnombre.Enabled = false;
+        }
+
+        private void txtid_Leave(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtid.Text);
+            using (var db = new RECIBOSEntities1())
+            {
+                var existeregistro = db.tblExamen.FirstOrDefault(x => x.idExamen == id);
+                if (existeregistro != null)
+                {
+                    MessageBox.Show("El registro ya existe, intente con otro");
+                    txtid.Text = "";
+                }
+            }
+        }
+
+
+    }
+}
+
